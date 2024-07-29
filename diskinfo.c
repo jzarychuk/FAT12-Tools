025df23e4dc732231fe2aa31c423d12656fa28d6
Jacob Arychuk <jza@linux200.csc.uvic.ca>
2024-07-29 14:55:00 -0700

    Move code from main to separate function in diskinfo.c

--- diskinfo.c
+++ diskinfo.c
@@ -15,6 +15,52 @@ int is_label_unchanged (char* label) {
 
 }
 
+// Function to get the label of the provided disk image
+char* get_label(FILE* file) {
+
+	// Seek to position of label in boot sector
+        if (fseek(file, LABEL_START_BYTE, SEEK_SET) != 0) { // Move file pointer LABEL_START_BYTE number of bytes relative to start of file
+                perror("Error seeking to label position of boot sector");
+                fclose(file);
+                exit(EXIT_FAILURE);
+        }
+
+        // Read the label from boot sector
+        char* label = (char*)calloc(LABEL_LENGTH_BYTES + 1, sizeof(char)); // Using calloc to avoid VLA
+        if (label == NULL) {
+                perror("Memory allocation failed");
+                fclose(file);
+                exit(EXIT_FAILURE);
+        }
+        if (fread(label, 1, LABEL_LENGTH_BYTES, file) != LABEL_LENGTH_BYTES) { // Read one byte at a time into label for LABEL_LENGTH_BYTES number of times
+                perror("Error reading label");
+                fclose(file);
+                exit(EXIT_FAILURE);
+        }
+
+	// If label not found, then check root directory
+        if (!is_label_unchanged(label)) {
+
+                long int root_dir_start_byte = ROOT_DIR_START_SECTOR * SECTOR_SIZE_BYTES;
+                int root_dir_length_bytes = (ROOT_DIR_END_SECTOR - ROOT_DIR_START_SECTOR + 1) * SECTOR_SIZE_BYTES;
+                char* entry;
+
+                // Iterate through each sector in root directory
+                for (int sector_offset = 0; sector_offset < root_dir_length_bytes; sector_offset += SECTOR_SIZE_BYTES) {
+                        entry = find_entry(file, 0x08, root_dir_start_byte + sector_offset);
+                        if (entry != NULL) {
+                                strncpy(label, entry, 11); // Copy the first 11 characters of the entry into the label
+                                free(entry);
+                                break;
+                        }
+                }
+
+        }		
+
+	return label;
+
+}
+
 int main (int argc, char* argv[]) {
 
 	if (argc < 2) {
@@ -49,44 +95,8 @@ int main (int argc, char* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	// Seek to position of label in boot sector
-	if (fseek(file, LABEL_START_BYTE, SEEK_SET) != 0) { // Move file pointer LABEL_START_BYTE number of bytes relative to start of file
-		perror("Error seeking to label position of boot sector");
-		fclose(file);
-		exit(EXIT_FAILURE);
-	}
-
-	// Read the label from boot sector
-	char* label = (char*)calloc(LABEL_LENGTH_BYTES + 1, sizeof(char)); // Using calloc to avoid VLA
-	if (label == NULL) {
-		perror("Memory allocation failed");
-		fclose(file);
-		exit(EXIT_FAILURE);
-	}
-	if (fread(label, 1, LABEL_LENGTH_BYTES, file) != LABEL_LENGTH_BYTES) { // Read one byte at a time into label for LABEL_LENGTH_BYTES number of times
-		perror("Error reading label");
-		fclose(file);
-		exit(EXIT_FAILURE);
-	}
-
-	// If label not found, then check root directory
-	if (!is_label_unchanged(label)) {
-
-		long int root_dir_start_byte = ROOT_DIR_START_SECTOR * SECTOR_SIZE_BYTES;
-		int root_dir_length_bytes = (ROOT_DIR_END_SECTOR - ROOT_DIR_START_SECTOR + 1) * SECTOR_SIZE_BYTES;
-		char* entry;
-
-		// Iterate through each sector in root directory
-		for (int sector_offset = 0; sector_offset < root_dir_length_bytes; sector_offset += SECTOR_SIZE_BYTES) {
-			entry = find_entry(file, 0x08, root_dir_start_byte + sector_offset);
-			if (entry != NULL) {
-				strncpy(label, entry, 11); // Copy the first 11 characters of the entry into the label
-				free(entry);
-				break;
-			}
-		}
-
-	}
+	// Get the label of the disk
+	char* label = get_label(file);
 
 	fprintf(stdout, "OS Name: %s\n", os_name);
 	fprintf(stdout, "Label of the disk: %s\n", label);
