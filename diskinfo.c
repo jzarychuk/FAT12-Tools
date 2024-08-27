6d998d4d15d34e154cc3e97ec676218f99d4104a
Jacob Arychuk <jza@linux200.csc.uvic.ca>
2024-08-27 08:54:41 -0700

    Refactored diskinfo.c to use a general function for reading data from boot sector

--- diskinfo.c
+++ diskinfo.c
@@ -5,70 +5,54 @@
 #include <stdint.h>
 #include <inttypes.h>
 
-// Function to get the OS name of the provided disk image
-char* get_os_name(FILE* file) {
+// Function to read a field of data from the boot sector of the provided disk image
+char* read_boot_sector_data(FILE* file, int start_byte, int length_bytes) {
 
-        // Seek to position of OS name in boot sector
-        if (fseek(file, OS_NAME_START_BYTE, SEEK_SET) != 0) { // Move file pointer OS_NAME_START_BYTE number of bytes relative to start of file
-                perror("Error seeking to OS name position of boot sector");
+        // Seek to position of the field in boot sector
+        if (fseek(file, start_byte, SEEK_SET) != 0) { // Move file pointer start_byte number of bytes relative to start of file
+                perror("Error seeking to the field in boot sector");
                 fclose(file);
                 exit(EXIT_FAILURE);
         }
 
-        // Read the OS name from boot sector
-        char* os_name = (char*)calloc(OS_NAME_LENGTH_BYTES + 1, sizeof(char)); // Using calloc to avoid VLA
-        if (os_name == NULL) {
+        // Allocate memory for string (while it may be for raw data, everything will be null-terminated for simplicity and safety)
+        char* data = (char*)calloc(length_bytes + 1, sizeof(char));
+        if (data == NULL) {
                 perror("Memory allocation failed");
                 fclose(file);
                 exit(EXIT_FAILURE);
         }
-        if (fread(os_name, 1, OS_NAME_LENGTH_BYTES, file) != OS_NAME_LENGTH_BYTES) { // Read one byte at a time into os_name for OS_NAME_LENGTH_BYTES number of times
-                perror("Error reading OS name");
+
+        // Read the field from boot sector
+        if (fread(data, 1, length_bytes, file) != length_bytes) { // Read one byte at a time into data for length_bytes number of times
+                perror("Error reading field from boot sector");
                 fclose(file);
                 exit(EXIT_FAILURE);
         }
 
-        return os_name;
+        return data;
 
 }
 
 // Function to check whether the char array for the label has changed from when it was initialized to all zeros
-int is_label_unchanged (char* label) {
+int is_label_changed (char* label) {
 
 	for (int i = 0; i < LABEL_LENGTH_BYTES; i++) {
-		if (label[i] != 0) {
-			return 0; // False, label is changed
+		if (label[i] != ' ') {
+			return 1; // True, label is changed
 		}
 	}
-	return 1; // True, label is unchanged
+	return 0; // False, label is unchanged
 
 }
 
 // Function to get the label of the provided disk image
 char* get_label(FILE* file) {
 
-	// Seek to position of label in boot sector
-        if (fseek(file, LABEL_START_BYTE, SEEK_SET) != 0) { // Move file pointer LABEL_START_BYTE number of bytes relative to start of file
-                perror("Error seeking to label position of boot sector");
-                fclose(file);
-                exit(EXIT_FAILURE);
-        }
-
-        // Read the label from boot sector
-        char* label = (char*)calloc(LABEL_LENGTH_BYTES + 1, sizeof(char)); // Using calloc to avoid VLA
-        if (label == NULL) {
-                perror("Memory allocation failed");
-                fclose(file);
-                exit(EXIT_FAILURE);
-        }
-        if (fread(label, 1, LABEL_LENGTH_BYTES, file) != LABEL_LENGTH_BYTES) { // Read one byte at a time into label for LABEL_LENGTH_BYTES number of times
-                perror("Error reading label");
-                fclose(file);
-                exit(EXIT_FAILURE);
-        }
+	char* label = read_boot_sector_data(file, LABEL_START_BYTE, LABEL_LENGTH_BYTES);
 
 	// If label not found, then check root directory
-        if (!is_label_unchanged(label)) {
+        if (!is_label_changed(label)) {
 
                 long int root_dir_start_byte = ROOT_DIR_START_SECTOR * SECTOR_SIZE_BYTES;
                 int root_dir_length_bytes = (ROOT_DIR_END_SECTOR - ROOT_DIR_START_SECTOR + 1) * SECTOR_SIZE_BYTES;
@@ -90,32 +74,12 @@ char* get_label(FILE* file) {
 
 }
 
-// Function to get the total sector count of the provided disk image
-uint16_t get_total_sector_count(FILE* file) {
-
-	// Seek to position of total sector count in boot sector
-	if (fseek(file, TOTAL_SECTOR_COUNT_START_BYTE, SEEK_SET) != 0) { // Move file pointer TOTAL_SECTOR_COUNT_START_BYTE number of bytes relative to start of file
-		perror("Error seeking to total sector count position of boot sector");
-		fclose(file);
-		exit(EXIT_FAILURE);
-	}
-
-	// Read the total sector count from boot sector
-	uint16_t total_sector_count;
-	if (fread(&total_sector_count, 1, TOTAL_SECTOR_COUNT_LENGTH_BYTES, file) != TOTAL_SECTOR_COUNT_LENGTH_BYTES) { // Read one byte at a time into total_sector_count for TOTAL_SECTOR_COUNT_LENGTH_BYTES number of times
-		perror("Error reading total sector count");
-		fclose(file);
-		exit(EXIT_FAILURE);
-	}
-
-	return total_sector_count;
-
-}
-
 // Function to get the unused sector count of the provided disk image
 int get_unused_sector_count(FILE* file) {
 
-	uint16_t total_sector_count = get_total_sector_count(file);
+	char* total_sector_count_data = read_boot_sector_data(file, TOTAL_SECTOR_COUNT_START_BYTE, TOTAL_SECTOR_COUNT_LENGTH_BYTES);
+        uint16_t total_sector_count = (uint16_t)((unsigned char)total_sector_count_data[0]) | (unsigned char)total_sector_count_data[1] << 8;
+        free(total_sector_count_data);
 	int unused_sector_count = 0;
 
         // Seek to position of first FAT table
@@ -234,52 +198,6 @@ int get_num_files(FILE* file, long int dir_start_byte, int dir_length_sectors) {
 
 }
 
-// Function to get the number of sectors per FAT in the provided disk image
-uint16_t get_sectors_per_fat(FILE* file){
-
-        uint16_t sectors_per_fat = 0;
-
-        // Seek to position of sectors per FAT in boot sector
-        if (fseek(file, SECTORS_PER_FAT_START_BYTE, SEEK_SET) != 0) { // Move file pointer SECTORS_PER_FAT_START_BYTE number of bytes relative to start of file
-                perror("Error seeking to sectors per FAT position of boot sector");
-                fclose(file);
-                exit(EXIT_FAILURE);
-        }
-
-        // Read the sectors per FAT from boot sector
-        if (fread(&sectors_per_fat, 1, SECTORS_PER_FAT_LENGTH_BYTES, file) != SECTORS_PER_FAT_LENGTH_BYTES) { // Read one byte at a time into sectors_per_fat for SECTORS_PER_FAT_LENGTH_BYTES number of times
-                perror("Error reading sectors per FAT");
-                fclose(file);
-                exit(EXIT_FAILURE);
-        }
-
-        return sectors_per_fat;
-
-}
-
-// Function to get the number of FAT copies in the provided disk image
-uint8_t get_num_fat_copies(FILE* file){
-
-        uint8_t num_fat_copies = 0;
-
-        // Seek to position of number of FAT copies in boot sector
-        if (fseek(file, NUM_FAT_COPIES_START_BYTE, SEEK_SET) != 0) { // Move file pointer NUM_FAT_COPIES_START_BYTE number of bytes relative to start of file
-                perror("Error seeking to number of FAT copies position of boot sector");
-                fclose(file);
-                exit(EXIT_FAILURE);
-        }
-
-        // Read the number of FAT copies from boot sector
-        if (fread(&num_fat_copies, 1, NUM_FAT_COPIES_LENGTH_BYTES, file) != NUM_FAT_COPIES_LENGTH_BYTES) { // Read one byte at a time into num_fat_copies for NUM_FAT_COPIES_LENGTH_BYTES number of times
-                perror("Error reading number of FAT copies");
-                fclose(file);
-                exit(EXIT_FAILURE);
-        }
-
-        return num_fat_copies;
-
-}
-
 int main (int argc, char* argv[]) {
 
 	if (argc < 2) {
@@ -295,13 +213,16 @@ int main (int argc, char* argv[]) {
 	}
 
 	// Get the OS name
-	char* os_name = get_os_name(file);
+	char* os_name = read_boot_sector_data(file, OS_NAME_START_BYTE, OS_NAME_LENGTH_BYTES);
 
 	// Get the label
 	char* label = get_label(file);
 
 	// Calculate the total size
-	float total_size = get_total_sector_count(file) * SECTOR_SIZE_BYTES;
+	char* total_sector_count_data = read_boot_sector_data(file, TOTAL_SECTOR_COUNT_START_BYTE, TOTAL_SECTOR_COUNT_LENGTH_BYTES);
+        uint16_t total_sector_count = (uint16_t)((unsigned char)total_sector_count_data[0]) | (unsigned char)total_sector_count_data[1] << 8;
+	free(total_sector_count_data);
+	float total_size = total_sector_count * SECTOR_SIZE_BYTES;
 
         // Calculate the free size
 	float free_size = get_unused_sector_count(file) * SECTOR_SIZE_BYTES;
@@ -312,10 +233,14 @@ int main (int argc, char* argv[]) {
 	int num_files = get_num_files(file, root_dir_start_byte, root_dir_length_sectors);
 	
         // Get the number of sectors per FAT
-        uint16_t sectors_per_fat = get_sectors_per_fat(file);
+        char* sectors_per_fat_data = read_boot_sector_data(file, SECTORS_PER_FAT_START_BYTE, SECTORS_PER_FAT_LENGTH_BYTES);
+	uint16_t sectors_per_fat = (uint16_t)((unsigned char)sectors_per_fat_data[0]) | (unsigned char)sectors_per_fat_data[1] << 8;
+	free(sectors_per_fat_data);
 
 	// Get the number of FAT copies
-	uint8_t num_fat_copies = get_num_fat_copies(file);
+	char* num_fat_copies_data = read_boot_sector_data(file, NUM_FAT_COPIES_START_BYTE, NUM_FAT_COPIES_LENGTH_BYTES);
+	uint8_t num_fat_copies = (uint8_t)(unsigned char)num_fat_copies_data[0];
+	free(num_fat_copies_data);
 
 	fprintf(stdout, "OS Name: %s\n", os_name);
 	fprintf(stdout, "Label of the disk: %s\n", label);
